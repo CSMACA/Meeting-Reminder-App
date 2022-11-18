0115a791f3409c463ad64e3496ad9ca2ded48a36
Alan Roach <arscience97@gmail.com>
2022-11-17 22:11:51 -0600

    Initial commit; Alarm properly executes at given time.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace Meeting_Reminder_Toture
 {
@@ -20,14 +21,36 @@ namespace Meeting_Reminder_Toture
     /// </summary>
     public partial class MainWindow : Window
     {
+        private string? reminderTime;
+        private string? currentTime;
         public MainWindow()
         {
             InitializeComponent();
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += timer_Tick;
+            timer.Start();
         }
 
         private void armAlarmButton_Click(object sender, RoutedEventArgs e)
         {
+            reminderTime = reminderTimeSet.Text + ":00";
+        }
 
+        private void equationSubmitButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            currentTime = DateTime.Now.ToLongTimeString();
+            if(currentTime == reminderTime && reminderTime != null)
+            {
+                //Call sound alarm function
+                Console.WriteLine("Reminder Time meets Current Time");
+                //Call puzzle selector function
+            }
         }
     }
 }
