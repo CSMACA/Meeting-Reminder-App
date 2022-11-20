fdf56486010e84046eb8911006b35dca9a9ca181
Alan Roach <arscience97@gmail.com>
2022-11-20 00:42:26 -0600

    Added full alarm functionality. Added one puzzle.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Media;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -13,9 +15,11 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Windows.Threading;
+using NCalc;
 
 namespace Meeting_Reminder_Toture
 {
+    
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
@@ -23,13 +27,17 @@ namespace Meeting_Reminder_Toture
     {
         private string? reminderTime;
         private string? currentTime;
+        
+        //sound path, default is instanced.
+        private static string soundPath = @"C:\Alarm\alarm.wav";
+        private SoundPlayer alarm = new SoundPlayer(soundPath);
         public MainWindow()
         {
-            InitializeComponent();
             DispatcherTimer timer = new DispatcherTimer();
             timer.Interval = TimeSpan.FromSeconds(1);
             timer.Tick += timer_Tick;
             timer.Start();
+            InitializeComponent();
         }
 
         private void armAlarmButton_Click(object sender, RoutedEventArgs e)
@@ -39,7 +47,13 @@ namespace Meeting_Reminder_Toture
 
         private void equationSubmitButton_Click(object sender, RoutedEventArgs e)
         {
+            TextBoxEmpty(equationAnswerEnter, "You must supply an answer.");
 
+            NCalc.Expression eq = new NCalc.Expression(equationText.Text);
+            if (equationAnswerEnter.Text == eq.Evaluate().ToString())
+            {
+                alarm.Stop();
+            }
         }
 
         private void timer_Tick(object sender, EventArgs e)
@@ -47,10 +61,53 @@ namespace Meeting_Reminder_Toture
             currentTime = DateTime.Now.ToLongTimeString();
             if(currentTime == reminderTime && reminderTime != null)
             {
-                //Call sound alarm function
-                Console.WriteLine("Reminder Time meets Current Time");
-                //Call puzzle selector function
+                //Call puzzle selector function (this function contains the stopAlarm function).
+                startRandomPuzzle();
             }
+        }
+        private void btnOpenFile_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "WAV Sound files (*.wav)|*.wav";
+            openFileDialog.ShowDialog();
+            soundPath = openFileDialog.FileName;
+            //reinitialize sound player to new sound path.
+            alarm = new SoundPlayer(soundPath);
+        }
+
+        private void startRandomPuzzle()
+        {
+            //start alarm
+            alarm.PlayLooping();
+
+            //for now, only picking one of the puzzles
+            //case when rand 1 - # of puzzles
+            equationPuzzle();
+        }
+
+        private void equationPuzzle()
+        {
+            Random r = new Random();
+
+            //Set random first number, second number, and sign.
+            int first = r.Next(0,99) ;
+            int second = r.Next(0,first);
+            //can be randomized between the 4 signs, just addition for now.
+            string sign = "+";
+
+            equationText.Text = String.Format("{0} {1} {2}",first,sign,second);
+        }
+
+        public bool TextBoxEmpty(TextBox txtBox, string displayMsg)
+        {
+            if (string.IsNullOrEmpty(txtBox.Text))
+            {
+                MessageBox.Show(displayMsg, "Required field", MessageBoxButton.OK, MessageBoxImage.Information);
+                txtBox.Focus();
+                return true;
+            }
+
+            return false;
         }
     }
 }
